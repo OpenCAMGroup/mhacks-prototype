d4099044ddc907c1d7a8d956e4f874542c102ff6
basel <basel@umich.edu>
2016-10-08 08:50:13 -0400

    include initial point in psotions list

--- Patterns.py
+++ Patterns.py
@@ -1,9 +1,7 @@
-import operator
-
 class Pattern:
     def __init__(self, geometry = None, initialPoint = [0,0]):
         self.initialPoint = initialPoint
-        self.positions = []
+        self.positions = [initialPoint]
         self.geometry = geometry
 
 class RegularLinearPattern(Pattern):
@@ -18,22 +16,22 @@ class RegularLinearPattern(Pattern):
             numPoints    = args[1]
             spacing      = args[2]
             super().__init__(geometry, initialPoint)
-            tempPos = [initialPoint]
-            tempPos.extend(pattern.positions)
-            for pos in tempPos:
+            self.positions = []
+
+            for pos in pattern.positions:
                 for j in range(numPoints+1):
                     self.positions.append( [pos[i] + (j) * spacing[i] for i in range(2)] )
-            self.positions.pop(0)
         else:
             geometry     = args[0]
             initialPoint = args[1]
             numPoints    = args[2]
             spacing      = args[3]
             super().__init__(geometry, initialPoint)
-            self.positions = [[initialPoint[i] + (x+1) * spacing[i] for i in range(2)] for x in range(numPoints)]
+            self.positions = [[initialPoint[i] + (x) * spacing[i] for i in range(2)] for x in range(numPoints+1)]
 
 class IrregularLinearPattern(Pattern):
     def toCumulative(self, spacings):
+        # turns a list of relative spacings into a list of cumulative spacings
         retList = []
         for i in range(len(spacings)):
             retList.append([sum([x[j] for x in spacings[:i+1]]) for j in range(2)])
@@ -47,15 +45,13 @@ class IrregularLinearPattern(Pattern):
             initialPoint = pattern.initialPoint
             spacings     = args[1]
             super().__init__(geometry, initialPoint)
+            self.positions = []
             
             cumuSpacings = [[0,0]]
             cumuSpacings.extend(self.toCumulative(spacings))
-            tempPos = [initialPoint]
-            tempPos.extend(pattern.positions)
-            for pos in tempPos:
+            for pos in pattern.positions:
                 for spacing in cumuSpacings:
                     self.positions.append([pos[i] + spacing[i] for i in range(2)])
-            self.positions.pop(0)
         else:
             geometry     = args[0]
             initialPoint = args[1]
